9102c7f3e86d01842ac87eb31e5d22f62d0a5c5a
David Wallace <david.wallace@gmail.com>
2012-10-02 00:00:48 -0700

    Add test for getting a post which has unicode

--- test_tentapp.py
+++ test_tentapp.py
@@ -6,11 +6,12 @@ from colors import *
 import testlib
 
 import tentapp
-tentapp.debug = False
+tentapp.debug = True
 
 
 print yellow('-----------------------------------------------------------------------\\')
 testlib.begin('TentApp')
+
 
 app = tentapp.TentApp('https://tent.tent.is')
 testlib.eq(   app.apiRootUrls, ['https://tent.tent.is/tent'], 'discovery should get the correct api root urls'   )
@@ -35,6 +36,13 @@ posts = app.getPosts()
 testlib.eq(   type(posts), list, 'posts should be a list'   )
 testlib.eq(   type(posts[0]), dict, 'posts should be a list of dicts'   )
 testlib.eq(   len(posts) > 10, True, 'this particular user should have more than 10 public posts'   )
+
+# Test unicode
+# This post has a unicode snowman in it: https://longears.tent.is/posts/hkl5ci
+longearsApp = tentapp.TentApp('https://longears.tent.is')
+post = longearsApp.getPosts(id='hkl5ci')
+testlib.eq(   u'\u2603' in post['content']['text'], True, 'unicode snowman should be present in this post'   )
+
 
 testlib.end()
 print yellow('-----------------------------------------------------------------------/')
